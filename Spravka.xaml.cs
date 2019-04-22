12728e2db9d2ad721a1dd0cebf3ac4e0fbaaceab
SirEleot <sireleot@gmail.com>
2019-04-22 09:54:12 +0300

    initional

---
+++ Spravka.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Kaliy3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Spravka : ContentPage
+    {
+        public Spravka()
+        {
+            InitializeComponent();
+            DateTime.Text = System.DateTime.Now.ToString("dd.MM.yyyy");
+        }
+        public void TrailNumber_completed(object sender, EventArgs e)
+        {
+            EntryLadenWagon.Focus();
+        }
+        public void LadenWagon_completed(object sender, EventArgs e)
+        {
+            EntryEmptyWagon.Focus();
+        }
+        public void EmptyWagon_completed(object sender, EventArgs e)
+        {
+            EntryTrailWeight.Focus();
+        }
+        public void TrailWeight_completed(object sender, EventArgs e)
+        {
+            EntryLastWagonNumber.Focus();
+        }
+        public void Calculate_click(object sender, EventArgs e)
+        {
+            int Laden = Convert.ToInt32(EntryLadenWagon.Text);
+            int LadenAxis = Laden * 4;
+            int Empty = Convert.ToInt32(EntryEmptyWagon.Text);
+            int EmptyAxis = Empty * 4;
+            if (EntryTrailNumber.Text == String.Empty || EntryTrailWeight.Text == String.Empty || !(Laden + Empty > 0))
+            {
+                DisplayAlert("Внимание!!!", $"Введены не все необходимые данные", "Ок");
+                return;
+            }
+            int Number = Convert.ToInt32(EntryTrailNumber.Text);
+            int Uklon = GetUklon(Number);
+            int Weight = Convert.ToInt32(EntryTrailWeight.Text);
+            string LastWagon = EntryLastWagonNumber.Text;
+
+            if (Uklon == 0) DisplayAlert("Внимание!!!", $"Не найден уклон для позда с номером {Number}. Скажите мне чтобы поправил!", "Ок");
+            int HandsBreak = (int)Math.Round(Weight * Uklon / 1000.0f);
+            if (HandsBreak % 2 > 0) HandsBreak--;
+            int Kof = Laden > 0 ? 33 : 55;
+            int Fact = (int)(LadenAxis * 7 + EmptyAxis * 3.5f);
+            int Needed = GetNeeded(Weight, Fact, ref Kof);
+            SpravkaResult Result = new SpravkaResult(Number, Weight, Needed, HandsBreak, EmptyAxis, LadenAxis, Kof, LastWagon);
+            Navigation.PushAsync(Result);
+        }
+        public void Reset_click(object sender, EventArgs e)
+        {
+            EntryLadenWagon.Text = "";
+            EntryEmptyWagon.Text = "";
+            EntryTrailNumber.Text = "";
+            EntryTrailWeight.Text = "";
+            EntryLastWagonNumber.Text = "";
+        }
+        private int GetUklon(int number)
+        {
+            if (number > 9500) return 6;
+            else if (number > 3639) return 7;
+            else if (number > 3609) return 4;
+            else if (number > 3499) return 4;
+            else if (number > 1649) return 8;
+            else return (0);
+        }
+        private int GetNeeded(int weight, int fact, ref int kof)
+        {
+            int counter = 0;
+            while (fact < weight * kof / 100)
+            {
+                kof--;
+                counter++;
+            }
+            if (counter > 3) DisplayAlert("Внимание!!!", $"Не хватает нажатия - проверь данные {fact} /{weight} / {weight * kof / 100} / {kof} / {counter}", "Ок");
+            return weight * kof / 100;
+        }
+    }
+}
---
+++ SpravkaResult.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Kaliy3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SpravkaResult : ContentPage
+    {
+        int[][] ConfigValue = new int[][]
+        {
+            new int[]{11,12,13,14,15,16},
+            new int[]{21,22,23,24,25,26},
+            new int[]{31,32,33,34,35,36},
+            new int[]{41,42,43,44,45,46},
+            new int[]{51,52,53,54,55,56}
+        };
+        int axis = 0;
+        public SpravkaResult(int number, int weight, int needed, int hands, int empty, int laden, int kof, string last)
+        {
+            InitializeComponent();
+            LabelNumber.Text = number.ToString();
+            LabelWeight.Text = weight.ToString();
+            LabelNeeded.Text = needed.ToString();
+            LabelHand.Text = hands.ToString();
+            LabelEmpty.Text = empty.ToString();
+            LabelEmptyAxis.Text = (empty * 3.5).ToString();
+            LabelLaden.Text = laden.ToString();
+            LabelLadenAxis.Text = (laden * 7).ToString();
+            axis = empty + laden;
+            LabelTotal.Text = (axis).ToString();
+            LabelTotalAxis.Text = (laden*7 + empty*3.5).ToString();
+            LabelKof.Text = $"100/{kof}";
+            LabelLast.Text = last;
+            LabelValue.Text = GetValueTrail(BtnTrail.Text, axis);
+        }
+
+        private string GetValueTrail(string text, int axis)
+        {
+            int index1;
+            int index2;
+            switch (text)
+            {
+                case "2ТЭ10У":
+                    index1 = 0;
+                    break;
+                case "2ТЭ10М":
+                    index1 = 1;
+                    break;
+                case "ЧМЭ3":
+                    index1 = 2;
+                    break;
+                case "М62":
+                    index1 = 3;
+                    break;
+                case "2М62":
+                    index1 = 4;
+                    break;
+                default: index1 = 0;
+                    break;
+            }
+            if (axis > 300) index2 = 0;
+            else if (axis > 250) index2 = 1;
+            else if (axis > 200) index2 = 2;
+            else if (axis > 150) index2 = 3;
+            else if (axis > 100) index2 = 4;
+            else index2 = 5;
+
+            return (ConfigValue[index1][index2]).ToString();
+        }
+
+        private async void SetTrail_click(object o, EventArgs e)
+        {
+            string Result = await DisplayActionSheet("Выбор тепловоза", "Отмена", null, "2ТЭ10У", "2ТЭ10М", "ЧМЭ3", "М62", "2М62");
+            BtnTrail.Text = Result;
+            LabelValue.Text = GetValueTrail(Result, axis);
+        }
+    }
+}
